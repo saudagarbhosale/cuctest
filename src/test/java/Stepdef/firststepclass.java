d5833b02398517db8efab8d83dd1f90297620782
Saudagar <saudagar.bhosale1@gmail.com>
2024-01-11 21:06:57 +0530

    change

--- src/test/java/Stepdef/firststepclass.java
+++ src/test/java/Stepdef/firststepclass.java
@@ -6,20 +6,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 public class firststepclass {
 	
 	
 	@Given("Pass url {string} b")
 	public void pass_url_b(String str) {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
 		driver.get("https://www.google.com/");
 		driver.close();
 	}
 	@Given("open url entername {string} a")
 	public void open_url_entername_a(String string) {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.close();
 	}
@@ -27,8 +28,8 @@ public class firststepclass {
 	@Given("open")
 	public void open()
 	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.findElement(By.xpath("//button[@class='abc']")).click();
 		driver.close();
